abb1ad00d6df8b766d650224cb4570b63df637dc
Esteban <esteban@peroz.fr>
2026-05-27 14:36:52 +0200

    feat: init repo
    Add cmakelists, basic class and main

---
+++ gui/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** main
+*/
+
+#include <iostream>
+
+// NOLINTNEXTLINE
+int main(int ac, char** av) {
+    std::cout << "Hey, I am a zappy gui!" << std::endl;
+    return 0;
+}
---
+++ tests/src/Demo.cpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Demo
+*/
+
+#include <gtest/gtest.h>
+
+TEST(Demo, RemoveMe) { EXPECT_EQ(1, 1); }
