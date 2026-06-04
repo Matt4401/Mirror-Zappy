273ea87474c4909720b067b9267d4bc55ea376d1
Esteban <esteban@peroz.fr>
2026-06-04 15:08:28 +0200

    feat(server/src/network/SessionManager.*): started creation of the SessionManager class

--- server/src/network/SessionManager.cpp
+++ server/src/network/SessionManager.cpp
@@ -4,3 +4,34 @@
 ** File description:
 ** Session manager
 */
+
+#include "SessionManager.hpp"
+
+#include <sys/poll.h>
+
+#include <cerrno>
+#include <cstdint>
+#include <stdexcept>
+
+namespace zappy::server::network {
+
+SessionManager::SessionManager(std::uint16_t port) {
+    _serverSocket.bindAndListen(port);
+    _pollFds.push_back({.fd = _serverSocket.fd(), .events = POLLIN});
+}
+
+void SessionManager::pollNetwork() {
+    const int readyFds = ::poll(_pollFds.data(), _pollFds.size(), 0);
+
+    if (readyFds < 0) {
+        if (errno == EINTR) {
+            return;
+        }
+        throw std::runtime_error{"fatal error during poll"};
+    }
+    if (readyFds == 0) {
+        return;
+    }
+}
+
+}  // namespace zappy::server::network
--- server/src/network/SessionManager.hpp
+++ server/src/network/SessionManager.hpp
@@ -7,19 +7,51 @@
 
 #pragma once
 
+#include <poll.h>
+#include <sys/poll.h>
+
+#include <cstdint>
+#include <queue>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
+
+#include "network/ClientSocket.hpp"
+#include "network/ServerSocket.hpp"
+
 namespace zappy::server::network {
 
 class SessionManager {
   public:
-    SessionManager() = default;
+    struct NetworkMessage {
+        int clientId;
+        std::string message;
+    };
+
+    explicit SessionManager(std::uint16_t port);
     ~SessionManager() = default;
 
     SessionManager(const SessionManager& other) = delete;
     SessionManager& operator=(const SessionManager& other) = delete;
-    SessionManager(SessionManager&& other) noexcept = default;
-    SessionManager& operator=(SessionManager&& other) noexcept = default;
+    SessionManager(SessionManager&& other) = delete;
+    SessionManager& operator=(SessionManager&& other) = delete;
+
+    void pollNetwork();
+    [[nodiscard]] bool tryPopMessage(NetworkMessage& message);
+    void sendMessage(int clientId, std::string_view message);
+
+    void disconnectClient(int clientId);
 
   private:
+    ServerSocket _serverSocket{};
+    std::vector<struct pollfd> _pollFds;
+
+    std::unordered_map<int, shared::network::ClientSocket> _clients;
+
+    std::unordered_map<int, std::string> _readBuffers;
+    std::unordered_map<int, std::string> _writeBuffers;
+    std::queue<NetworkMessage> _messageQueue;
 };
 
 }  // namespace zappy::server::network
