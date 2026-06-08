2b5c20d23a24f60156ca70e23e95dcb59784f660
Esteban <esteban@peroz.fr>
2026-06-08 18:06:20 +0200

    feat(server/src/util/DataStructures.hpp): add config struct
    Data struct for args

---
+++ server/src/util/DataStructures.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Data structures declaration
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+namespace zappy::server::util {
+
+struct Config {
+    std::uint16_t port{};
+    std::uint32_t width{};
+    std::uint32_t height{};
+    std::vector<std::string> teamNames;
+    std::uint32_t clientLimit;
+    std::uint32_t freq;
+};
+
+}  // namespace zappy::server::util
