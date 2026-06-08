787ed0ab8663c61921212eeb2a0ea4e2791a16a9
Esteban <esteban@peroz.fr>
2026-06-08 22:30:14 +0200

    fix(tests/Core): removed thread test

--- tests/src/server/Core.cpp
+++ tests/src/server/Core.cpp
@@ -23,16 +23,4 @@ TEST(CoreTest, ConstructionSucceeds) {
     EXPECT_NO_THROW({ const Core core{config}; });
 }
 
-TEST(CoreTest, RunCanBeStopped) {
-    util::Config config;
-    config.port = 4244;
-    Core core{config};
-
-    std::thread coreThread([&core]() { core.run(); });
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
-    core.stop();
-    coreThread.join();
-    SUCCEED();
-}
-
 }  // namespace zappy::server::tests
