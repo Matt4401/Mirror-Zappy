df06ae2486e3350c9a08fe63fb6392717a4ab492
eliott raguin <eliott.raguin@epitech.eu>
2026-06-09 19:02:15 +0200

    fix(color): use raylib color instead of mine

--- gui/raylib/Color.hpp
+++ gui/raylib/Color.hpp
@@ -21,32 +21,32 @@ class Color {
     Color(Color&& other) noexcept = default;
     Color& operator=(Color&& other) noexcept = default;
 
-    static constexpr Color LightGray() { return {200, 200, 200, 255}; }
-    static constexpr Color Gray() { return {130, 130, 130, 255}; }
-    static constexpr Color DarkGray() { return {80, 80, 80, 255}; }
-    static constexpr Color Yellow() { return {253, 249, 0, 255}; }
-    static constexpr Color Gold() { return {255, 203, 0, 255}; }
-    static constexpr Color Orange() { return {255, 161, 0, 255}; }
-    static constexpr Color Pink() { return {255, 109, 194, 255}; }
-    static constexpr Color Red() { return {230, 41, 55, 255}; }
-    static constexpr Color Maroon() { return {190, 33, 55, 255}; }
-    static constexpr Color Green() { return {0, 228, 48, 255}; }
-    static constexpr Color Lime() { return {0, 158, 47, 255}; }
-    static constexpr Color DarkGreen() { return {0, 117, 44, 255}; }
-    static constexpr Color SkyBlue() { return {102, 191, 255, 255}; }
-    static constexpr Color Blue() { return {0, 121, 241, 255}; }
-    static constexpr Color DarkBlue() { return {0, 82, 172, 255}; }
-    static constexpr Color Purple() { return {200, 122, 255, 255}; }
-    static constexpr Color Violet() { return {135, 60, 190, 255}; }
-    static constexpr Color DarkPurple() { return {112, 31, 126, 255}; }
-    static constexpr Color Beige() { return {211, 176, 131, 255}; }
-    static constexpr Color Brown() { return {127, 106, 79, 255}; }
-    static constexpr Color DarkBrown() { return {76, 63, 47, 255}; }
-    static constexpr Color White() { return {255, 255, 255, 255}; }
-    static constexpr Color Black() { return {0, 0, 0, 255}; }
-    static constexpr Color Blank() { return {0, 0, 0, 0}; }
-    static constexpr Color Magenta() { return {255, 0, 255, 255}; }
-    static constexpr Color RayWhite() { return {245, 245, 245, 255}; }
+    static constexpr Color LightGray() { return LIGHTGRAY; }
+    static constexpr Color Gray() { return GRAY; }
+    static constexpr Color DarkGray() { return DARKGRAY; }
+    static constexpr Color Yellow() { return YELLOW; }
+    static constexpr Color Gold() { return GOLD; }
+    static constexpr Color Orange() { return ORANGE; }
+    static constexpr Color Pink() { return PINK; }
+    static constexpr Color Red() { return RED; }
+    static constexpr Color Maroon() { return MAROON; }
+    static constexpr Color Green() { return GREEN; }
+    static constexpr Color Lime() { return LIME; }
+    static constexpr Color DarkGreen() { return DARKGREEN; }
+    static constexpr Color SkyBlue() { return SKYBLUE; }
+    static constexpr Color Blue() { return BLUE; }
+    static constexpr Color DarkBlue() { return DARKBLUE; }
+    static constexpr Color Purple() { return PURPLE; }
+    static constexpr Color Violet() { return VIOLET; }
+    static constexpr Color DarkPurple() { return DARKPURPLE; }
+    static constexpr Color Beige() { return BEIGE; }
+    static constexpr Color Brown() { return BROWN; }
+    static constexpr Color DarkBrown() { return DARKBROWN; }
+    static constexpr Color White() { return WHITE; }
+    static constexpr Color Black() { return BLACK; }
+    static constexpr Color Blank() { return BLANK; }
+    static constexpr Color Magenta() { return MAGENTA; }
+    static constexpr Color RayWhite() { return RAYWHITE; }
 
     [[nodiscard]] constexpr ::Color color() const { return _color; }
 
