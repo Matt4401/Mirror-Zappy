5a220a3d88fe69f63db3fb0fdb66929690966361
eliott raguin <eliott.raguin@epitech.eu>
2026-06-09 19:01:18 +0200

    feat(color): add constexpr for all colors

--- gui/raylib/Color.hpp
+++ gui/raylib/Color.hpp
@@ -7,32 +7,53 @@
 
 #pragma once
 #include <raylib.h>
-#include <sys/types.h>
 
 #include <cstdint>
 
 namespace zappy::gui::raylib {
 class Color {
   public:
-    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) : _color(r, g, b, a) {}
-    Color(::Color color) : _color{color} {}
+    constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 255) : _color(r, g, b, a) {}
+    constexpr Color(::Color color) : _color{color} {}
     ~Color() = default;
     Color(const Color& other) = default;
     Color& operator=(const Color& other) = default;
-    Color(Color&& other) noexcept : _color{other._color} {}
-    Color& operator=(Color&& other) noexcept {
-        if (this != &other) {
-            _color = other._color;
-        }
-        return *this;
-    }
+    Color(Color&& other) noexcept = default;
+    Color& operator=(Color&& other) noexcept = default;
 
-    [[nodiscard]] ::Color color() const { return _color; }
+    static constexpr Color LightGray() { return {200, 200, 200, 255}; }
+    static constexpr Color Gray() { return {130, 130, 130, 255}; }
+    static constexpr Color DarkGray() { return {80, 80, 80, 255}; }
+    static constexpr Color Yellow() { return {253, 249, 0, 255}; }
+    static constexpr Color Gold() { return {255, 203, 0, 255}; }
+    static constexpr Color Orange() { return {255, 161, 0, 255}; }
+    static constexpr Color Pink() { return {255, 109, 194, 255}; }
+    static constexpr Color Red() { return {230, 41, 55, 255}; }
+    static constexpr Color Maroon() { return {190, 33, 55, 255}; }
+    static constexpr Color Green() { return {0, 228, 48, 255}; }
+    static constexpr Color Lime() { return {0, 158, 47, 255}; }
+    static constexpr Color DarkGreen() { return {0, 117, 44, 255}; }
+    static constexpr Color SkyBlue() { return {102, 191, 255, 255}; }
+    static constexpr Color Blue() { return {0, 121, 241, 255}; }
+    static constexpr Color DarkBlue() { return {0, 82, 172, 255}; }
+    static constexpr Color Purple() { return {200, 122, 255, 255}; }
+    static constexpr Color Violet() { return {135, 60, 190, 255}; }
+    static constexpr Color DarkPurple() { return {112, 31, 126, 255}; }
+    static constexpr Color Beige() { return {211, 176, 131, 255}; }
+    static constexpr Color Brown() { return {127, 106, 79, 255}; }
+    static constexpr Color DarkBrown() { return {76, 63, 47, 255}; }
+    static constexpr Color White() { return {255, 255, 255, 255}; }
+    static constexpr Color Black() { return {0, 0, 0, 255}; }
+    static constexpr Color Blank() { return {0, 0, 0, 0}; }
+    static constexpr Color Magenta() { return {255, 0, 255, 255}; }
+    static constexpr Color RayWhite() { return {245, 245, 245, 255}; }
 
-    [[nodiscard]] uint8_t r() const { return _color.r; }
-    [[nodiscard]] uint8_t g() const { return _color.g; }
-    [[nodiscard]] uint8_t b() const { return _color.b; }
-    [[nodiscard]] uint8_t a() const { return _color.a; }
+    [[nodiscard]] constexpr ::Color color() const { return _color; }
+
+    [[nodiscard]] constexpr std::uint8_t r() const { return _color.r; }
+    [[nodiscard]] constexpr std::uint8_t g() const { return _color.g; }
+    [[nodiscard]] constexpr std::uint8_t b() const { return _color.b; }
+    [[nodiscard]] constexpr std::uint8_t a() const { return _color.a; }
 
   protected:
   private:
--- gui/src/graphics/Tile3D.cpp
+++ gui/src/graphics/Tile3D.cpp
@@ -7,12 +7,15 @@
 
 #include "Tile3D.hpp"
 
+#include "Color.hpp"
 #include "rmath/Vector3.hpp"
 
 namespace zappy::gui::graphics {
 
 Tile3D::Tile3D() : _model{MODEL_PATH} {}
 
-void Tile3D::draw(raylib::rmath::Vector3 position) const { _model.drawModel(position, 1.0F, WHITE); }
+void Tile3D::draw(raylib::rmath::Vector3 position) const {
+    _model.drawModel(position, 1.0F, raylib::Color::White().color());
+}
 
 }  // namespace zappy::gui::graphics
