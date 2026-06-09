9377f40ba62a644ea03a5e5175d082df892957df
Noah Savoye <noah.savoye@epitech.eu>
2026-06-09 18:04:22 +0200

    feat: basic tests for the main in ia part

---
+++ tests/src/ai/test_main.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import pytest
+from unittest.mock import patch
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../ai/src')))
+
+from AITeamClass import AITeam
+from zappy_ai import main
+
+
+
+
+def test_main():
+    test_args = ["zappy_ai", "-p", "4242", "-n", "EquipeTest", "-h", "localhost"]
+    with patch("sys.argv", test_args):
+        main()
+
+def test_invalide_main_zero_args():
+    test_wrong_args = ["zappy_ai"]
+
+    with patch('sys.argv', test_wrong_args):
+        with pytest.raises(SystemExit) as excinfo:
+            main()
+        assert excinfo.value.code == 84
+
+def test_invalide_main_zero_args():
+    test_wrong_args = ["zappy_ai", "-p", "4242", "-n", "EquipeTest", "-h"]
+
+    with patch('sys.argv', test_wrong_args):
+        with pytest.raises(SystemExit) as excinfo:
+            main()
+        assert excinfo.value.code == 84
