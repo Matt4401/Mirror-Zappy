8c42ad31a5f463cb3fe3a6e6f5014a8faa3ecccc
eliott raguin <eliott.raguin@epitech.eu>
2026-06-09 22:06:44 +0200

    fix(Client): format file to clang format 21

--- gui/src/network/Client.cpp
+++ gui/src/network/Client.cpp
@@ -11,9 +11,9 @@
 #include "exception/SocketError.hpp"
 
 namespace zappy::gui::network {
-    Client::Client(const cli::Parser::Config& config) : _socket{config.machine, config.port} {
-        if (_socket.send(DefaultTeamName) == -1) {
-            throw shared::exception::SocketError("Failed to send GRAPHIC command");
-        }
+Client::Client(const cli::Parser::Config& config) : _socket{config.machine, config.port} {
+    if (_socket.send(DefaultTeamName) == -1) {
+        throw shared::exception::SocketError("Failed to send GRAPHIC command");
     }
+}
 }  // namespace zappy::gui::network
