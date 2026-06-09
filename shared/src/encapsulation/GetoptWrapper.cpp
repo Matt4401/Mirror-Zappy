96474480fa60339123f6736602dcbb0c81340ba5
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-09 21:03:18 +0200

    feat(GetoptWrapper): added of an encapsulation of getopt C function for parsing

---
+++ shared/src/encapsulation/GetoptWrapper.cpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** GetoptWrapper
+*/
+
+#include "encapsulation/GetoptWrapper.hpp"
+
+#include <unistd.h>
+
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace zappy::shared::encapsulation {
+GetOptWrapper::GetOptWrapper(const int argc, char** argv, std::string optString)
+    : _argc(argc), _argv(argv), _optString(std::move(optString)) {
+    optind = 1;
+    opterr = 0;
+    for (int i = 0; i < argc; ++i) {
+        _stringArgv.emplace_back(*std::next(argv, i));
+    }
+}
+
+int GetOptWrapper::getNextOption() const { return getopt(_argc, _argv, _optString.c_str()); }
+
+std::string GetOptWrapper::getOptionArg() {
+    if (optarg == nullptr) {
+        return "";
+    }
+    return static_cast<std::string>(optarg);
+}
+
+std::vector<std::string> GetOptWrapper::getMultiArgs() const {
+    std::vector<std::string> args;
+
+    if (optarg != nullptr) {
+        args.emplace_back(optarg);
+    }
+    while (optind < _argc && _stringArgv.at(optind).at(0) != '-') {
+        args.emplace_back(_stringArgv.at(optind));
+        optind++;
+    }
+    return args;
+}
+
+char GetOptWrapper::getUnknownOption() { return static_cast<char>(optopt); }
+}  // namespace zappy::shared::encapsulation
