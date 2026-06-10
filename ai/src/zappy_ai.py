96ded471a06cfbefbd8aa351052137cc2c183c98
Noah Savoye <noah.savoye@epitech.eu>
2026-06-10 13:34:40 +0200

    fix(zappy_ai): format *.py

--- ai/src/zappy_ai.py
+++ ai/src/zappy_ai.py
@@ -4,28 +4,28 @@ import argparse
 from AITeamClass import AITeam
 from TrantorianClass import Trantorian
 
+
 def main():
     parser = argparse.ArgumentParser(
-        usage="./zappy_ai -p port -n name -h machine",
-        add_help=False
+        usage="./zappy_ai -p port -n name -h machine", add_help=False
     )
     parser.add_value_provider = False
-    parser.add_argument('--help', action='help', help='Show this help message -> exit')
-    parser.add_argument('-p', type=int, required=True, metavar='port')
-    parser.add_argument('-n', type=str, required=True, metavar='name')
-    parser.add_argument('-h', type=str, required=True, metavar='machine')
+    parser.add_argument("--help", action="help", help="Show this help message -> exit")
+    parser.add_argument("-p", type=int, required=True, metavar="port")
+    parser.add_argument("-n", type=str, required=True, metavar="name")
+    parser.add_argument("-h", type=str, required=True, metavar="machine")
 
     try:
         args = parser.parse_args()
     except SystemExit:
         sys.exit(84)
-    port = args.p# noqa: F841
-    name = args.n# noqa: F841
+    port = args.p  # noqa: F841
+    name = args.n  # noqa: F841
     machine = args.h
 
     trantorian = Trantorian("matt4401")
-    main_class = AITeam(trantorian, machine)# noqa: F841
-    #main_class.run()
+    main_class = AITeam(trantorian, machine)  # noqa: F841
+    # main_class.run()
 
 
 if __name__ == "__main__":
