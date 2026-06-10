ab6208b1480872595b35c6323bf2f30d331c6a94
Tristan Fragnaud <tristan.fragnaud@epitech.eu>
2026-06-10 11:28:47 +0200

    feat(Left): implement Left command for player orientation adjustment

---
+++ server/src/command/Left.cpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2026
+** zappy
+** File description:
+** Left
+*/
+
+#include "Left.hpp"
+
+#include "command/ACommand.hpp"
+#include "game/Player.hpp"
+
+namespace zappy::server::command {
+
+Left::Left() : ACommand{kTimeLimit} {}
+
+void Left::execute(game::World& /*world*/, game::Player& player) {
+    setRequiredTicks(kTimeLimit);
+
+    auto newOrientation = static_cast<std::uint8_t>(player.getOrientation()) - 1;
+    if (newOrientation < 0) {
+        newOrientation = 3;
+    }
+    player.setOrientation(static_cast<game::orientation>(newOrientation));
+    player.addResponse("ok\n");
+}
+}  // namespace zappy::server::command
