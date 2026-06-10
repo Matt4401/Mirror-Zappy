0cacbb813350ba143a4e5c8a8019743fd49d497b
eliott raguin <eliott.raguin@epitech.eu>
2026-06-10 23:53:14 +0200

    fix(cmake): remove useless package on cmake

--- cmake/SetupGoogleTest.cmake
+++ cmake/SetupGoogleTest.cmake
@@ -4,11 +4,7 @@ include(InternetCheck)
 check_internet_connection()
 
 macro(setup_googletest)
-    find_package(GTest QUIET)
-
-    if (TARGET GTest::gtest AND TARGET GTest::gmock AND TARGET GTest::gmock_main)
-        message(STATUS "GoogleTest found on system. Unit tests will be built.")
-    elseif (INTERNET_AVAILABLE)
+    if (INTERNET_AVAILABLE)
         FetchContent_Declare(
                 googletest
                 GIT_REPOSITORY https://github.com/google/googletest.git
@@ -18,17 +14,7 @@ macro(setup_googletest)
         set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 
         FetchContent_MakeAvailable(googletest)
-
-        if (TARGET gtest AND NOT TARGET GTest::gtest)
-            add_library(GTest::gtest ALIAS gtest)
-        endif()
-        if (TARGET gmock AND NOT TARGET GTest::gmock)
-            add_library(GTest::gmock ALIAS gmock)
-        endif()
-        if (TARGET gmock_main AND NOT TARGET GTest::gmock_main)
-            add_library(GTest::gmock_main ALIAS gmock_main)
-        endif()
     else()
-        message(STATUS "GoogleTest not available. Unit tests will not be built.")
+        message(WARNING "GoogleTest not available. Unit tests will not be built.")
     endif()
 endmacro()
