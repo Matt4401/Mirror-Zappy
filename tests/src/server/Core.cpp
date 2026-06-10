73a8d77a6d69b3470c52811c522a1aacc7679059
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-10 22:02:21 +0200

    fix(tests): fix core unit tests with new main logic

--- tests/src/server/Core.cpp
+++ tests/src/server/Core.cpp
@@ -9,15 +9,35 @@
 
 #include <gtest/gtest.h>
 
+#include <span>
+#include <string>
+#include <vector>
+
 #include "parsing/strategy/ServerStrategy.hpp"
+
+namespace {
+
+std::vector<char*> buildArgv(std::vector<std::string>& args) {
+    std::vector<char*> argv;
+
+    argv.reserve(args.size());
+    for (std::string& arg : args) {
+        argv.push_back(arg.data());
+    }
+    return argv;
+}
+
+}  // namespace
 
 namespace zappy::server::tests {
 
 TEST(CoreTest, ConstructionSucceeds) {
-    zappy::parser::parsing::ServerConfig config;
-    config.port = 4243;
+    std::vector<std::string> args{"./zappy_server", "-p", "4243", "-x", "10", "-y", "10", "-n", "TeamA",
+                                  "-c",             "1"};
+    auto argv = buildArgv(args);
+    const std::span<char*> argvSpan{argv.data(), argv.size()};
 
-    EXPECT_NO_THROW({ const Core core{config}; });
+    EXPECT_NO_THROW({ const Core core{argvSpan}; });
 }
 
 }  // namespace zappy::server::tests
