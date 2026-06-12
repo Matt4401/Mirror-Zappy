8f0f3735226048da322af314a70e9d199053c3a4
TomGatin <tom.gatin@epitech.eu>
2026-06-12 02:09:25 +0200

    feat(core): add msz gui protocol parsing to server analysis (temporary)

--- server/src/Core.cpp
+++ server/src/Core.cpp
@@ -10,10 +10,15 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <utility>
+#include <variant>
 
 #include "SessionManager.hpp"
 #include "network/ISessionManager.hpp"
+#include "protocol/Commands.hpp"
+#include "protocol/Emitter.hpp"
+#include "protocol/Parser.hpp"
 #include "util/DataStructures.hpp"
 
 namespace zappy::server {
@@ -31,10 +36,28 @@ void Core::run() {
         try {
             _sessionManager->pollNetwork(_timeUnit);
             while (_sessionManager->tryPopMessage(message)) {
-                // TODO: should be removed, is of course temporary for testing purposes
-                std::cout << "Received message from client " << message.clientId
-                          << " of type: " << static_cast<int>(message.type) << ". Message: " << message.message
-                          << std::endl;
+                // TODO: Example of parsing the command (to be moved to a proper CommandHandler)
+                shared::protocol::Parser const parser;
+                auto clientCmd = parser.parseClientCommand(message.message);
+
+                if (std::holds_alternative<shared::protocol::client::Msz>(
+                        clientCmd)) {  // TODO: instead of using holds_alternative, we should use std::visit with
+                                       // an overloaded lambda to avoid multiple parsing of the same command
+                    std::cout << "Client " << message.clientId << " requested MSZ." << std::endl;
+
+                    shared::protocol::server::Msz responseCmd{};
+                    responseCmd.width = 16;   // TODO: get real map size from world
+                    responseCmd.height = 16;  // TODO: get real map size from world
+                    std::string const responseStr = shared::protocol::Emitter::build(responseCmd);
+
+                    _sessionManager->sendMessage(message.clientId, responseStr);
+                } else if (std::holds_alternative<shared::protocol::UnknownCommand>(clientCmd)) {
+                    std::string const responseStr = shared::protocol::Emitter::build(shared::protocol::server::Suc{});
+                    _sessionManager->sendMessage(message.clientId, responseStr);
+                } else {
+                    std::cout << "Received other message from client " << message.clientId << ": " << message.message
+                              << std::endl;
+                }
             }
         } catch (const std::exception& e) {
             std::cerr << "Error in main loop: " << e.what() << std::endl;
