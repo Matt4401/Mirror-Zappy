cbbd5f57cc5ad96c7d15c39ec005144b9f3f927a
TomGatin <tom.gatin@epitech.eu>
2026-06-13 14:31:10 +0200

    feat(tests): add EventDispatcher unit tests for subscription and dispatch functionality

---
+++ tests/src/gui/events/EventDispatcherTest.cpp
@@ -0,0 +1,64 @@
+/*
+** EPITECH PROJECT, 2026
+** Mirror-Zappy
+** File description:
+** EventDispatcherTest
+*/
+
+#include <gtest/gtest.h>
+
+#include "../../../../gui/src/events/EventDispatcher.hpp"
+#include "protocol/Commands.hpp"
+
+using namespace zappy::gui::events;
+using namespace zappy::shared::protocol;
+
+TEST(EventDispatcherTest, SubscribeAndDispatch) {
+    EventDispatcher dispatcher;
+    int receivedWidth = 0;
+    int receivedHeight = 0;
+
+    auto token = dispatcher.subscribe<server::Msz>([&](const server::Msz& cmd) {
+        receivedWidth = cmd.width;
+        receivedHeight = cmd.height;
+    });
+
+    ServerCommand const cmd = server::Msz{10, 20};
+    dispatcher.dispatch(cmd);
+
+    EXPECT_EQ(receivedWidth, 10);
+    EXPECT_EQ(receivedHeight, 20);
+}
+
+TEST(EventDispatcherTest, Unsubscribe) {
+    EventDispatcher dispatcher;
+    bool called = false;
+
+    auto token = dispatcher.subscribe<server::Msz>([&](const server::Msz&) { called = true; });
+
+    dispatcher.unsubscribe<server::Msz>(token);
+
+    ServerCommand const cmd = server::Msz{10, 20};
+    dispatcher.dispatch(cmd);
+
+    EXPECT_FALSE(called);
+}
+
+TEST(EventDispatcherTest, MultipleListenersAndDifferentCommands) {
+    EventDispatcher dispatcher;
+    int mszCount = 0;
+    int pnwCount = 0;
+
+    auto token1 = dispatcher.subscribe<server::Msz>([&](const server::Msz&) { mszCount++; });
+    auto token2 = dispatcher.subscribe<server::Msz>([&](const server::Msz&) { mszCount++; });
+    auto token3 = dispatcher.subscribe<server::Pnw>([&](const server::Pnw&) { pnwCount++; });
+
+    ServerCommand cmdMsz = server::Msz{.width = 10, .height = 20};
+    dispatcher.dispatch(cmdMsz);
+
+    ServerCommand cmdPnw = server::Pnw{.playerId = 1, .x = 5, .y = 5, .orientation = 1, .level = 1, .teamName = "team"};
+    dispatcher.dispatch(cmdPnw);
+
+    EXPECT_EQ(mszCount, 2);
+    EXPECT_EQ(pnwCount, 1);
+}
