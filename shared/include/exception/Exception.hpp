3eda4fe152569b704ef72dfafaa0d2954379f932
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-16 22:11:13 +0200

    refactor(Exception.hpp): change exception system with using of vformat and variadic args

--- shared/include/exception/Exception.hpp
+++ shared/include/exception/Exception.hpp
@@ -8,18 +8,33 @@
 #pragma once
 
 #include <exception>
+#include <source_location>
 #include <string>
 
 namespace zappy::shared::exception {
 
+using source = std::source_location;
+
 class Exception : public std::exception {
-  public:
-    explicit Exception(std::string message);
+public:
+    explicit Exception(std::string_view,
+                       const source &location = source::current());
+    Exception(const Exception &) = default;
+    Exception(Exception &&) = default;
+    ~Exception() override = default;
 
-    [[nodiscard]] const char* what() const noexcept override;
+    Exception &operator=(const Exception &) = default;
+    Exception &operator=(Exception &&) = default;
 
-  private:
+    const char *what() const noexcept override;
+    const source &location() const noexcept;
+
+protected:
     std::string _message;
+    std::source_location _location;
+    std::string _fullMessage;
+
+    [[nodiscard]] std::string buildFullMessage() const;
 };
 
 }  // namespace zappy::shared::exception
