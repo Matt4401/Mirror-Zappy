9bf437cc803beac7a3acf8240443ef66d979ddcf
Noah Savoye <noah.savoye@epitech.eu>
2026-06-16 22:42:56 +0200

    fix(CLient): format AITeamClass with ruff

--- ai/src/AITeamClass.py
+++ ai/src/AITeamClass.py
@@ -10,9 +10,7 @@ class AITeam:
         self.answer_list = []
         self.data_lock = threading.Lock()
         self.team_name = team_name
-        self.connection = AIConnection(
-            host, port, team_name
-        )
+        self.connection = AIConnection(host, port, team_name)
         self.player_state = PlayerState(team_name)
 
     def run(self):
