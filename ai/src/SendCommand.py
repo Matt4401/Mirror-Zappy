d98c5c4cb41aca7b158543d85428d8f2f7bb00ec
Noah Savoye <noah.savoye@epitech.eu>
2026-06-17 17:03:03 +0200

    fix(CLient): remove "send_" in methods name

--- ai/src/SendCommand.py
+++ ai/src/SendCommand.py
@@ -2,38 +2,38 @@ class SendCommand:
     def __init__(self, connection):
         self.connection = connection
 
-    def send_forward(self):
+    def forward(self):
         self.connection.send_connection("Forward")
 
-    def send_right(self):
+    def right(self):
         self.connection.send_connection("Right")
 
-    def send_left(self):
+    def left(self):
         self.connection.send_connection("Left")
 
-    def send_look(self):
+    def look(self):
         self.connection.send_connection("Look")
 
-    def send_inventory(self):
+    def inventory(self):
         self.connection.send_connection("Inventory")
 
-    def send_broadcast(self, txt):
+    def broadcast(self, txt):
         self.connection.send_connection("Broadcast <" + txt + ">")
 
-    def send_available_team_slots(self):
+    def available_team_slots(self):
         self.connection.send_connection("Connect_nbr")
 
-    def send_fork(self):
+    def fork(self):
         self.connection.send_connection("Fork")
 
-    def send_eject(self):
+    def eject(self):
         self.connection.send_connection("Eject")
 
-    def send_take_object(self, obj):
+    def take_object(self, obj):
         self.connection.send_connection("Take <" + obj + ">")
 
-    def send_set_object_down(self, obj):
+    def set_object_down(self, obj):
         self.connection.send_connection("Set <" + obj + ">")
 
-    def send_start_incantation(self):
+    def start_incantation(self):
         self.connection.send_connection("Incantation")
