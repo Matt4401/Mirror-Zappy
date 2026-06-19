428a2f9acf5d0958d1997ef7547026278fde31a5
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-19 14:09:49 +0200

    refactor(InitLogger.py): put the logger into a class instead of just functions

--- ai/src/util/InitLogger.py
+++ ai/src/util/InitLogger.py
@@ -4,32 +4,38 @@ import logging.config
 from pathlib import Path
 
 
-def setup_player_logging(player_id: str, config, DOSSIER_LOGS):
-    name_handler = f"player_{player_id}_file"
-    name_logger = f"player_{player_id}"
-    path_file_log = DOSSIER_LOGS / f"player_{player_id}.log"
+class PlayerLogger:
 
-    template_handler = config['handlers'].pop('player_file_template', None)
-    if template_handler:
-        config['handlers'][name_handler] = copy.deepcopy(template_handler)
-        config['handlers'][name_handler]['class'] = 'logging.handlers.RotatingFileHandler'
-        config['handlers'][name_handler]['filename'] = str(path_file_log)
+    @staticmethod
+    def setup_player_logging(player_id: str, config, DOSSIER_LOGS):
+        name_handler = f"player_{player_id}_file"
+        name_logger = f"player_{player_id}"
 
-    config['loggers'][name_logger] = copy.deepcopy(config['loggers']['player_template'])
-    config['loggers'][name_logger]['handlers'].append(name_handler)
+        dossier_players = DOSSIER_LOGS / "player"
+        dossier_players.mkdir(parents=True, exist_ok=True)
+        path_file_log = dossier_players / f"player_{player_id}.log"
 
+        template_handler = config['handlers'].pop('player_file_template', None)
+        if template_handler:
+            config['handlers'][name_handler] = copy.deepcopy(template_handler)
+            config['handlers'][name_handler]['class'] = 'logging.handlers.RotatingFileHandler'
+            config['handlers'][name_handler]['filename'] = str(path_file_log)
 
-def setup_logging(player_id: str):
-    RACINE_PROJECT = Path(__file__).resolve().parent
-    DOSSIER_LOGS = RACINE_PROJECT / "logs"
-    DOSSIER_LOGS.mkdir(parents=True, exist_ok=True)
+        config['loggers'][name_logger] = copy.deepcopy(config['loggers']['player_template'])
+        config['loggers'][name_logger]['handlers'].append(name_handler)
 
-    with open(RACINE_PROJECT / "logger_config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+    @staticmethod
+    def setup_logging(player_id: str):
+        RACINE_PROJECT = Path(__file__).resolve().parent
+        DOSSIER_LOGS = RACINE_PROJECT / "logs"
+        DOSSIER_LOGS.mkdir(parents=True, exist_ok=True)
 
-    setup_player_logging(player_id, config, DOSSIER_LOGS)
+        with open(RACINE_PROJECT / "logger_config.yaml", "r") as f:
+            config = yaml.safe_load(f)
 
-    config['handlers']['network_file']['filename'] = str(DOSSIER_LOGS / "server.log")
-    config['handlers']['command_file']['filename'] = str(DOSSIER_LOGS / "commands.log")
+        PlayerLogger.setup_player_logging(player_id, config, DOSSIER_LOGS)
 
-    logging.config.dictConfig(config)
+        config['handlers']['network_file']['filename'] = str(DOSSIER_LOGS / "server.log")
+        config['handlers']['command_file']['filename'] = str(DOSSIER_LOGS / "commands.log")
+
+        logging.config.dictConfig(config)
