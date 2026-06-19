76315c20062031576970008e4ab32dd4440d9f67
TomGatin <tom.gatin@epitech.eu>
2026-06-19 18:57:21 +0200

    fix(camera): Improve Camera constructor initialization and enable move semantics

--- gui/raylib/rcore/Camera.hpp
+++ gui/raylib/rcore/Camera.hpp
@@ -19,12 +19,15 @@ class Camera {
     static constexpr float MIN_DISTANCE = 2.0F;
     static constexpr float VISIBILITY_ORIGIN_BACKSTEP = 4.0F;
 
+    Camera() : _camera{} {}
     Camera(rmath::Vector3 position);
+    Camera(rmath::Vector3 position, rmath::Vector3 target, rmath::Vector3 up, float fovy, int projection)
+        : _camera{position.vector(), target.vector(), up.vector(), fovy, projection} {}
     ~Camera() = default;
     Camera(const Camera& other) = delete;
     Camera& operator=(const Camera& other) = delete;
-    Camera(Camera&& other) = delete;
-    Camera& operator=(Camera&& other) = delete;
+    Camera(Camera&& other) noexcept = default;
+    Camera& operator=(Camera&& other) noexcept = default;
 
     [[nodiscard]] Camera3D camera() const { return _camera; }
     [[nodiscard]] rmath::Vector3 position() const { return _camera.position; }
