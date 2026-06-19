472bdda8cd85cd3e2a6c5e0bca6bd6dd7af99c52
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-19 13:51:51 +0200

    fix(logger): fix template in logger by rewrite them in python

--- ai/src/util/InitLogger.py
+++ ai/src/util/InitLogger.py
@@ -4,18 +4,22 @@ import logging.config
 from pathlib import Path
 
 
-def setup_player_logging(player_id: str, team_name: str, config, DOSSIER_LOGS):
-    name_handler = f"player_{team_name}_{player_id}_file"
-    name_logger = f"player_{team_name}_{player_id}"
+def setup_player_logging(player_id: str, config, DOSSIER_LOGS):
+    name_handler = f"player_{player_id}_file"
+    name_logger = f"player_{player_id}"
     path_file_log = DOSSIER_LOGS / f"player_{player_id}.log"
 
-    config['handlers'][name_handler] = copy.deepcopy(config['handlers']['player_file_template'])
-    config['handlers'][name_handler]['filename'] = str(path_file_log)
+    template_handler = config['handlers'].pop('player_file_template', None)
+    if template_handler:
+        config['handlers'][name_handler] = copy.deepcopy(template_handler)
+        config['handlers'][name_handler]['class'] = 'logging.handlers.RotatingFileHandler'
+        config['handlers'][name_handler]['filename'] = str(path_file_log)
+
     config['loggers'][name_logger] = copy.deepcopy(config['loggers']['player_template'])
     config['loggers'][name_logger]['handlers'].append(name_handler)
 
 
-def setup_logging(player_id: str, team_name: str):
+def setup_logging(player_id: str):
     RACINE_PROJECT = Path(__file__).resolve().parent
     DOSSIER_LOGS = RACINE_PROJECT / "logs"
     DOSSIER_LOGS.mkdir(parents=True, exist_ok=True)
@@ -23,7 +27,7 @@ def setup_logging(player_id: str, team_name: str):
     with open(RACINE_PROJECT / "logger_config.yaml", "r") as f:
         config = yaml.safe_load(f)
 
-    setup_player_logging(player_id, team_name, config, DOSSIER_LOGS)
+    setup_player_logging(player_id, config, DOSSIER_LOGS)
 
     config['handlers']['network_file']['filename'] = str(DOSSIER_LOGS / "server.log")
     config['handlers']['command_file']['filename'] = str(DOSSIER_LOGS / "commands.log")
