b12cb4d9f29642afa26900e21b1e521a1e1be11f
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-23 15:37:25 +0200

    fix(SurviveState): update command sending calls by using send_command

--- ai/src/fsm/states/SurviveState.py
+++ ai/src/fsm/states/SurviveState.py
@@ -7,12 +7,12 @@ class SurviveState(AState):
 
         if closest_food_idx is not None:
             # self.trantorian.move_to_tile(closest_food_idx) # Fct de pathfinding de noah (pas encore push)
-            self.trantorian.connexion.send_command("Take <food>")
+            self.trantorian.send_command.take_object("food")
 
         else:
-            look_resp = self.trantorian.connexion.send_command("Look")
+            look_resp = self.trantorian.send_command.look()
             self.trantorian.vision = look_resp
 
         closest_food_idx = self.trantorian.vision.get_tile_index_of("food")
         if closest_food_idx is None:
-            self.trantorian.connexion.send_command("Forward")
+            self.trantorian.send_command.forward()
