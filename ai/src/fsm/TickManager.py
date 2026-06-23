d067ebf620e7b254059735385f5af2b9d3a8f824
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-23 02:30:58 +0200

    feat(TickManager): add the class who will be in charge of the tick

---
+++ ai/src/fsm/TickManager.py
@@ -0,0 +1,31 @@
+from .Constant import LOOK_INTERVAL
+from .Constant import INVENTORY_INTERVAL
+from .Constant import BROADCAST_INTERVAL
+
+
+class TickManager:
+    def __init__(self, agent_id: int):
+        self.tick = 0
+        self.broadcast_offset = (agent_id * 7) % 50
+
+    def tick_update(self) -> list[str]:
+        cmds = []
+        self.tick += 1
+
+        if self._should_inventory():
+            cmds.append("Inventory")
+        if self._should_look():
+            cmds.append("Look")
+        if self._should_broadcast():
+            cmds.append(None)
+
+        return cmds
+
+    def _should_inventory(self) -> bool:
+        return self.tick % INVENTORY_INTERVAL == 0
+
+    def _should_look(self) -> bool:
+        return self.tick % LOOK_INTERVAL == 0
+
+    def _should_broadcast(self) -> bool:
+        return (self.tick + self.broadcast_offset) % BROADCAST_INTERVAL == 0
