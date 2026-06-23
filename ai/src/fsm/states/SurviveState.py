47ac78a17ec087fa7ce64e90030df5ae222a6a5d
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-06-23 11:59:25 +0200

    fix(SurviveState): refactor function name for searching food in vision

--- ai/src/fsm/states/SurviveState.py
+++ ai/src/fsm/states/SurviveState.py
@@ -3,7 +3,7 @@ from ..AState import AState
 
 class SurviveState(AState):
     def execute(self):
-        closest_food_idx = self.trantorian.vision.get_closest_food("food")
+        closest_food_idx = self.trantorian.vision.get_tile_index_of("food")
 
         if closest_food_idx is not None:
             # self.trantorian.move_to_tile(closest_food_idx) # Fct de pathfinding de noah (pas encore push)
@@ -13,6 +13,6 @@ class SurviveState(AState):
             look_resp = self.trantorian.connexion.send_command("Look")
             self.trantorian.vision = look_resp
 
-        closest_food_idx = self.trantorian.vision.get_closest_food("food")
+        closest_food_idx = self.trantorian.vision.get_tile_index_of("food")
         if closest_food_idx is None:
             self.trantorian.connexion.send_command("Forward")
