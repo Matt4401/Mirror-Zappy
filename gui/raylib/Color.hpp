fb1e5b4f045a40129b3196e04ab49cf380183b19
TomGatin <tom.gatin@epitech.eu>
2026-06-24 00:01:20 +0200

    fix(Color): correct initialization syntax in Color constructor

--- gui/raylib/Color.hpp
+++ gui/raylib/Color.hpp
@@ -14,7 +14,7 @@ namespace zappy::gui::raylib {
 class Color {
   public:
     constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a = 255) noexcept
-        : _color(r, g, b, a) {}
+        : _color{r, g, b, a} {}
     constexpr Color(::Color color) noexcept : _color{color} {}
     ~Color() = default;
     Color(const Color& other) = default;
