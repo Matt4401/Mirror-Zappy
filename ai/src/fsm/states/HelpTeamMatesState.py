95172f4af6389c10cb043199508865a03dd6783f
Noah Savoye <noah.savoye@epitech.eu>
2026-06-25 00:02:13 +0200

    feat(CLient): format HelpTeamMatesState.py with ruff

--- ai/src/fsm/states/HelpTeamMatesState.py
+++ ai/src/fsm/states/HelpTeamMatesState.py
@@ -1,10 +1,13 @@
 from ..AState import AState
 from src.util.BroadcastMessage import BroadcastMessage
 
+
 class HelpTeamMatesState(AState):
     def __init__(self, trantorian):
         super().__init__(trantorian)
-        self.trantorian.logger.info("===========Entering Help Team mates state===========")
+        self.trantorian.logger.info(
+            "===========Entering Help Team mates state==========="
+        )
         self.broadcast_decoder = BroadcastMessage(self.trantorian.player_state)
         self.target_direction = 0
 
@@ -17,12 +20,12 @@ class HelpTeamMatesState(AState):
             if decoded is not None:
                 level, command = decoded
                 self.target_direction = direction
-                self.trantorian.logger.info(
-                    f"[HelpTeamMates] Wer found a team mate"
-                )
+                self.trantorian.logger.info(f"[HelpTeamMates] Wer found a team mate")
                 break
         if self.target_direction > 0:
-            self.trantorian.logger.info(f"[HelpTeamMates] going to {self.target_direction}")
+            self.trantorian.logger.info(
+                f"[HelpTeamMates] going to {self.target_direction}"
+            )
             self.trantorian.move_one_step_toward(self.target_direction)
             self.target_direction = 0
 
