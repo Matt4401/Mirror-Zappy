a36cb8c9eead7c8521fb97e2b60b6c4a19b089e3
Esteban Peroz <esteban@peroz.fr>
2026-06-26 14:15:12 +0200

    fix(server/logger): removed unused logging functions

--- server/src/Logger.cpp
+++ server/src/Logger.cpp
@@ -12,11 +12,7 @@
 
 namespace zappy::server {
 
-void Logger::logDebug(std::string_view message) { std::cout << "[SERVER] [DEBUG] " << message << std::endl; }
-
 void Logger::logInfo(std::string_view message) { std::cout << "[SERVER] [INFO] " << message << std::endl; }
-
-void Logger::logWarning(std::string_view message) { std::cout << "[SERVER] [WARNING] " << message << std::endl; }
 
 void Logger::logError(std::string_view message) { std::cerr << "[SERVER] [ERROR] " << message << std::endl; }
 
--- server/src/Logger.hpp
+++ server/src/Logger.hpp
@@ -12,9 +12,7 @@ namespace zappy::server {
 
 class Logger {
   public:
-    static void logDebug(std::string_view message);
     static void logInfo(std::string_view message);
-    static void logWarning(std::string_view message);
     static void logError(std::string_view message);
 };
 
