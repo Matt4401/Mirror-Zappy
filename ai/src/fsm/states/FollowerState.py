ca8ff0afc038f285af6d54413f8b6f9eb275d61e
Noah Savoye <noah.savoye@epitech.eu>
2026-06-27 02:11:07 +0200

    fix(CLient): add the folllowerstate skeletton

--- ai/src/fsm/states/FollowerState.py
+++ ai/src/fsm/states/FollowerState.py
@@ -3,10 +3,25 @@ from ..AState import AState
 
 class FollowerState(AState):
     def execute(self):
-        pass
-        # if broadcast message == pierre
-        # dès qu'on en a une, on va dessus et pose
-        # if broadcast message == join un certain level
-        # on rejoint avec la direction jusqu'à arriver à  broadcast[direction == 0, "..."]
-        # else
-        # broadcast : on est là, la famille
+        self.trantorian.logger.info("===========Entering Follower state===========")
+        if not self.trantorian.received_broadcasts:
+            self.trantorian.send_command.forward() # dans le cas on a pas de broadcast, on est obligé d'en chercher un sinon on est pas follower
+
+        latest_broadcast = self.trantorian.received_broadcasts.pop(0)
+        direction = latest_broadcast.get("dir")
+        raw_message = latest_broadcast.get("msg")
+        decoded = self.trantorian.broadcast_manager.read_broadcast(raw_message)
+        if decoded:
+            sender_id, instruction = decoded
+            if "join" in instruction:
+                if direction == 0:
+                    self.trantorian.logger.info("[Follower]: On leader emplacement")
+                    self.trantorian.broadcast_manager.create_message(self.trantorian.broadcast_manager.id, "") # pas besoin d'instruction ici normlaement
+                else:
+                    self.trantorian.logger.info(f"[Follower]: To leader emplacement (direction {direction})")
+                    self.trantorian.move_one_step_toward(direction)
+            elif "drop" in instruction:
+                pass # la faut jeter la pierre demandée + broadast pour informer le leade
+
+
+        # il manque juste la gestion de la demande du leader pour une pierre, je pense il faut un état au follower genre searchin' stone, giving stone)
