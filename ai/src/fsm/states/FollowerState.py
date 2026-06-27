4c677209ad8f9f6dee8acc06c1877c62277570a8
TomGatin <tom.gatin@epitech.eu>
2026-06-27 20:00:55 +0200

    fix(Client): enhance FollowerState logic for stone handling and broadcasting

--- ai/src/fsm/states/FollowerState.py
+++ ai/src/fsm/states/FollowerState.py
@@ -1,6 +1,7 @@
 import random
 from ..AState import AState
 
+
 class FollowerState(AState):
     def execute(self):
         self.trantorian.logger.info("===========Entering Follower state===========")
@@ -18,28 +19,61 @@ class FollowerState(AState):
                 clean_instruction = instruction.replace("need ", "").strip()
                 stone_list = clean_instruction.split(" ")
                 for stone in stone_list:
-                    if self.trantorian.player_state.vision.get_tile_index_of(stone) == 0:
+                    if (
+                        self.trantorian.player_state.vision.get_tile_index_of(stone)
+                        == 0
+                    ):
                         self.trantorian.take_object(stone)
-                        #self.trantorian.refresh_inventory()
-                if self.trantorian.traveling_stone is None:
-                    stone = random.choice(stone_list)
-                    tile_index = self.trantorian.player_state.vision.get_tile_index_of(
-                        stone
-                    )
-                    if tile_index is not None:
-                        self.trantorian.move_to_tile(tile_index)
-                        res = self.trantorian.take_object(stone)
-                        if res and res[0]:
-                            val = getattr(
+                        # self.trantorian.refresh_inventory()
+                has_needed_stone = False
+                for stone in stone_list:
+                    if getattr(self.trantorian.player_state.inventory, stone, 0) > 0:
+                        has_needed_stone = True
+                        break
+                if has_needed_stone:
+                    if direction == 0:
+                        for stone in [
+                            "linemate",
+                            "deraumere",
+                            "sibur",
+                            "mendiane",
+                            "phiras",
+                            "thystame",
+                        ]:
+                            count = getattr(
                                 self.trantorian.player_state.inventory, stone, 0
                             )
-                            setattr(
-                                self.trantorian.player_state.inventory, stone, val + 1
-                            )
-                    return
+                            for _ in range(count):
+                                res = self.trantorian.set_object_down(stone)
+                                if res and res[0]:
+                                    val = getattr(
+                                        self.trantorian.player_state.inventory, stone, 0
+                                    )
+                                    setattr(
+                                        self.trantorian.player_state.inventory,
+                                        stone,
+                                        val - 1,
+                                    )
+                    else:
+                        self.trantorian.move_one_step_toward(direction)
                 else:
-                    if direction == 0:
-                        self.trantorian.set_object_down(self.trantorian.traveling_stone)
+                    if self.trantorian.traveling_stone is None:
+                        stone = random.choice(stone_list)
+                        tile_index = (
+                            self.trantorian.player_state.vision.get_tile_index_of(stone)
+                        )
+                        if tile_index is not None:
+                            self.trantorian.move_to_tile(tile_index)
+                            res = self.trantorian.take_object(stone)
+                            if res and res[0]:
+                                val = getattr(
+                                    self.trantorian.player_state.inventory, stone, 0
+                                )
+                                setattr(
+                                    self.trantorian.player_state.inventory,
+                                    stone,
+                                    val + 1,
+                                )
                         return
                     self.trantorian.move_one_step_toward(direction)
                 return
@@ -48,16 +82,18 @@ class FollowerState(AState):
                 if level != self.trantorian.player_state.level:
                     return
                 if direction == 0:
-                    self.trantorian.logger.info("[Follower]: On leader emplacement")
-                    msg = self.trantorian.broadcast_manager.create_message(
-                        self.trantorian.status, "Here"
-                    )
-                    self.trantorian.send_command.broadcast(msg)
+                    if self.fsm.tick_manager._should_broadcast():
+                        self.trantorian.logger.info("[Follower]: On leader emplacement")
+                        msg = self.trantorian.broadcast_manager.create_message(
+                            self.trantorian.status, "Here"
+                        )
+                        self.trantorian.send_command.broadcast(msg)
                 else:
                     self.trantorian.logger.info(
                         f"[Follower]: To leader emplacement (direction {direction})"
                     )
                     self.trantorian.move_one_step_toward(direction)
+
 
 # faire en sorte de gratter ressources sur le chemin -> fait
 # rajouter if de est ce qu'on est concerné par le moove
